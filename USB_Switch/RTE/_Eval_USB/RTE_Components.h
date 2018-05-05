84825475a7470a655c50b1920714eab7f19bb7b8
tripger <tripger3@gmail.com>
2018-05-04 20:01:40 -0400

    Added initial files. Not even finished adding BSP files.

---
+++ USB_Switch/RTE/_Eval_USB/RTE_Components.h
@@ -0,0 +1,36 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'USB_Switch' 
+ * Target:  'Eval_USB' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l4xx.h"
+
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+#define RTE_DEVICE_HAL_ADC
+#define RTE_DEVICE_HAL_COMMON
+#define RTE_DEVICE_HAL_CORTEX
+#define RTE_DEVICE_HAL_DMA
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_I2C
+#define RTE_DEVICE_HAL_PWR
+#define RTE_DEVICE_HAL_RCC
+#define RTE_DEVICE_HAL_SRAM
+#define RTE_DEVICE_STARTUP_STM32L4XX    /* Device Startup for STM32L4 */
+
+#endif /* RTE_COMPONENTS_H */
